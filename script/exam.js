ac36ce37600853c73b28cf518b2afd982ce56263
shawarma-man <87611022+shawarma-man@users.noreply.github.com>
2021-12-01 23:01:51 +0300

    Add files via upload

---
+++ script/exam.js
@@ -0,0 +1,7 @@
+let header = document.getElementById("header_content_1");
+const typed = new Typed(".animate", {
+    strings: ["Creativity", "Ambitiousness","CarEvo"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+});

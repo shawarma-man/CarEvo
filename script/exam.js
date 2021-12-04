9254098473634fa88d1c12230ee14f29808af2d3
shawarma-man <87611022+shawarma-man@users.noreply.github.com>
2021-12-04 03:46:56 +0300

    Delete script directory

--- script/exam.js
+++
@@ -1,7 +0,0 @@
-let header = document.getElementById("header_content_1");
-const typed = new Typed(".animate", {
-    strings: ["Creativity", "Ambitiousness","CarEvo"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    loop: true,
-});

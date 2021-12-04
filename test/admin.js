d0b96337aeb2b850b15e3589154b3504eb859e1c
shawarma-man <87611022+shawarma-man@users.noreply.github.com>
2021-12-05 00:30:30 +0300

    Add files via upload

--- test/admin.js
+++ test/admin.js
@@ -2,30 +2,63 @@ let users_btn=document.getElementById("users-btn");
 let cars_btn=document.getElementById("cars-btn");
 let pending_btn=document.getElementById("pending-btn");
 let history_btn=document.getElementById("history-btn");
+let profile_btn=document.getElementById("profile-btn");
 users_btn.addEventListener("click",swap);
 cars_btn.addEventListener("click",swap);
 pending_btn.addEventListener("click",swap);
 history_btn.addEventListener("click",swap);
+profile_btn.addEventListener("click",swap);
 let msg = "dumb fuck"
 function swap(){
     if(this.id=="users-btn"){
+        $(".profile-panel").hide();
+        $(".data").show();
+        $(".Grops").show();
         $(".da").hide();
         $("#users").show();
         $(".title").text("Registered Users");
     }
     else if(this.id=="cars-btn"){
+        $(".profile-panel").hide();
+        $(".data").show();
+        $(".Grops").show();
         $(".da").hide();
         $("#cars").show();
         $(".title").text("Cars in the inventory");
     }
     else if(this.id=="pending-btn"){
+        $(".profile-panel").hide();
+        $(".data").show();
+        $(".Grops").show();
         $(".da").hide();
         $("#pending").show();
         $(".title").text("Pending orders");
     }
     else if(this.id=="history-btn"){
+        $(".profile-panel").hide();
+        $(".data").show();
+        $(".Grops").show();
         $(".da").hide();
         $("#history").show();
         $(".title").text("Recent Orders");
     }
+    else if(this.id=="profile-btn"){
+        $(".data").hide();
+        $(".Grops").hide();
+        $(".profile-panel").show();
+    }
+}
+
+ourfile = document.getElementById('img-input');
+preview = document.getElementById('profile-img');
+
+fr = new FileReader();
+
+ourfile.onchange = function () {
+    file = ourfile.files[0];
+    fr.readAsDataURL(file);
+}
+
+fr.onload = function(){
+    preview.src = fr.result;
 }
--- test/profile.js
+++ test/profile.js
@@ -1,20 +1,37 @@
-var bgImageArray = ["backgroundnew.jpg", "background.jpg"],
-    base = "";
-secs = 4;
-bgImageArray.forEach(function(img){
-    new Image().src = base + img;
-    // caches images, avoiding white flash between background replacements
-});
 
-function backgroundSequence() {
-    window.clearTimeout();
-    var k = 0;
-    for (i = 0; i < bgImageArray.length; i++) {
-        setTimeout(function(){
-            document.body.style.backgroundImage = "url(" + base + bgImageArray[k] + ")";
-            document.body.style.backgroundSize ="cover";
-            if ((k + 1) === bgImageArray.length) { setTimeout(function() { backgroundSequence() }, (secs * 1000))} else { k++; }
-        }, (secs * 1000) * i)
+let cars_btn=document.getElementById("cars-btn");
+
+let profile_btn=document.getElementById("profile-btn");
+
+cars_btn.addEventListener("click",swap);
+
+profile_btn.addEventListener("click",swap);
+function swap(){
+    if(this.id=="cars-btn"){
+        $(".profile-panel").hide();
+        $(".data").show();
+        $(".Grops").show();
+        $(".da").hide();
+        $("#cars").show();
+        $(".title").text("Owned Cars");
+    }
+    else if(this.id=="profile-btn"){
+        $(".data").hide();
+        $(".Grops").hide();
+        $(".profile-panel").show();
     }
 }
-backgroundSequence();
+
+ourfile = document.getElementById('img-input');
+preview = document.getElementById('profile-img');
+
+fr = new FileReader();
+
+ourfile.onchange = function () {
+    file = ourfile.files[0];
+    fr.readAsDataURL(file);
+}
+
+fr.onload = function(){
+    preview.src = fr.result;
+}
